0da66c706273c106b0ec13e351f5d86fa44ae45a
gozdetumacar <gstumacar@st.medipol.edu.tr>
2021-06-02 20:32:23 +0300

    tamamlandı

---
+++ consoleProgramlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace consoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba "+ name + " " + surname);
+
+        }
+    }
+}
